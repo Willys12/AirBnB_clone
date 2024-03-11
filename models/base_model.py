4dab8b6481955fb03882a01b7225ccc0a8de643c
Willys12 <ben.willys92@gmail.com>
2024-03-11 15:49:26 +0300

    Pycodestyle documentation

--- models/base_model.py
+++ models/base_model.py
@@ -6,7 +6,6 @@ from datetime import datetime
 class BaseModel:
     """Base model class for the common methods"""
 
-
     def __init__(self, *args, **kwargs):
         """Initializes Basemodel"""
         if kwargs:
@@ -23,20 +22,18 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
         if 'created_at' in kwargs:
-            self.created_at = datetime.strptime(kwargs['created_at'], 
+            self.created_at = datetime.strptime(kwargs['created_at'],
                                                 "%Y-%m-%dT%H:%M:%S.%f")
         else:
             self.created_at = datetime.now()
         if 'updated_at' in kwargs:
-            self.updated_at = datetime.strptime(kwargs['updated_at'], 
+            self.updated_at = datetime.strptime(kwargs['updated_at'],
                                                 "%Y-%m-%dT%H:%M:%S.%f")
 
-
     def save(self):
-        """updates the public instance attribute updated_at with 
+        """updates the public instance attribute updated_at with
         the current datetime"""
         self.updated_at = datetime.now()
-
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values"""
@@ -47,9 +44,9 @@ class BaseModel:
         obj_dict['__class__'] = self.__class__.__name__
         return obj_dict
 
-
     def __str__(self):
         """Returns string rep. of object"""
 
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
+        
